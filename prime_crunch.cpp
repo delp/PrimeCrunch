71e53b07fd31e3b14c023152a4405f1b4744d4ad
Alex R. Delp <delpa@seattleu.edu>
2015-01-23 10:58:53 -0800

    Test commit argggh

---
+++ prime_crunch.cpp
@@ -0,0 +1,40 @@
+//v0.3
+
+#include <iostream>
+#include <fstream>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+  ofstream datafile;
+  datafile.open("primes.txt", ios::app);
+  datafile << 2 << endl;
+  int number = 3;
+  while(true)
+    {
+      bool isPrime = true;;
+      for(int i = 2; i <= sqrt(number); i++)
+	{
+	  if(number % i == 0)
+	    {
+	      isPrime = false;
+	    }
+	}
+
+      if(isPrime)
+	{
+	  datafile << number << endl;
+	}
+      if(number == 2147483647)
+	{
+	  return -1;
+	}
+      number++;
+      number++;
+    }  
+  datafile.close();
+
+  return 0;
+}
